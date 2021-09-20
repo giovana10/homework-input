98fc2e7cb5dcfba4c7ca1236ecbf637ccedcd0e1
giovana10 <88749160+giovana10@users.noreply.github.com>
2021-09-20 19:20:13 -0300

    Add files via upload

---
+++ giovana_210920.py
@@ -0,0 +1,11 @@
+print("Question 2 ************************")
+clear= input("Write a phrase, please ")
+print("Question 3 ************************")
+for value in range(10):
+    print(clear + "!")
+print("Question 4 ************************")
+sunday= int(input("Can you say a number, please ? "))
+wednesday= int(input("Another ? "))
+print("Question 5 ************************")
+print("The sum of these numbers is " + str(sunday+wednesday))
+
